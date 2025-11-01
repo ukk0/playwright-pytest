e55c71e2511944909053c0c87f507192e2fd0e6f
ukk0 <sami.ukk91@gmail.com>
2025-11-01 20:18:25 +0100

    navigation tests

--- pages/navigation_page.py
+++ pages/navigation_page.py
@@ -1,4 +1,4 @@
-from playwright.sync_api import Page, Playwright
+from playwright.sync_api import Page, Playwright, expect
 from pages.base_page import BasePage
 
 
@@ -7,6 +7,7 @@ class NavigationPage(BasePage):
         super().__init__(page, playwright)
 
         self.hamburger_menu = page.get_by_role("button", name="Open Menu")
+        self.menu_wrapper = page.locator("[class='bm-menu-wrap']")
         self.menu_all_items_button = page.get_by_role(role="link", name="All Items")
         self.menu_about_button = page.get_by_role(role="link", name="About")
         self.menu_logout_button = page.get_by_role(role="link", name="Logout")
@@ -15,6 +16,8 @@ class NavigationPage(BasePage):
 
     def open_side_menu(self):
         self.hamburger_menu.click()
+        expect(self.menu_wrapper).to_be_visible()
 
     def close_side_menu(self):
         self.close_menu_button.click()
+        expect(self.menu_wrapper).not_to_be_visible()
